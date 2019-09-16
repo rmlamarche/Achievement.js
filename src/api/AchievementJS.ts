468d3e92a296915dfebc7458092c69ec82501bec
rmlamarche <rmlamarche@wpi.edu>
2019-09-16 11:31:49 -0400

    updates

--- src/api/AchievementJS.ts
+++ src/api/AchievementJS.ts
@@ -6,6 +6,8 @@ import AchievementJSConfig, { MongoURI } from '../interfaces/configs/Achievement
 import Achievement from '../models/Achievement';
 import AchievementsAPI from './achievements/AchievementsAPI';
 import UsersAPI from './users/UsersAPI';
+import { ObjectId } from 'bson';
+import { Op, Statistic } from '../enums';
 
 export default class AchievementJS {
 
@@ -25,57 +27,74 @@ export default class AchievementJS {
     this._usersAPI = new UsersAPI();
 
     const mongoURI = config != null && typeof config.MongoURI === 'string' ? config.MongoURI : `mongodb://${(config.MongoURI as MongoURI).user}:${(config.MongoURI as MongoURI).password}@${(config.MongoURI as MongoURI).host}:${(config.MongoURI as MongoURI).port}/${(config.MongoURI as MongoURI).database}`;
-    mongoose.connect(mongoURI, { useNewUrlParser: true }).catch((err) => {
+    mongoose.connect(mongoURI, { useNewUrlParser: true }).catch(err => {
       console.error(err);
     });
 
     const middleware = (req: Request, res: Response, next: NextFunction) => {
-      Achievement.findOne(
-        {
-          'meta.isActive': true,
-          '$and': [
-            {
-              $or: [
-                {
-                  'meta.expiration': {
-                    $lte: new Date(),
-                  },
-                },
-                {
-                  'meta.expiration': null,
-                },
-              ],
-            },
-            {
-              $or: [
-                {
-                  action: req.url.split(config.scope).slice(1)[0].split('?').shift(),
-                },
-                {
-                  action: `/${req.url.split(config.scope).slice(1)[0].split('?').shift()}`,
-                },
-              ],
-            },
-          ],
-        },
-        ).exec((err, achievement) => {
-          if (err) {
-            return next(err);
+      // check for achievment with action on scope
+      const action: string = req.url.split(config.scope).slice(1)[0].split('?').shift();
+      this.api.achievements.findActiveAchievementByAction(action).then(achievement => {
+        if (!achievement) {
+          return next(new Error('action not found'));
+        }
+        // check for user
+        const userID: ObjectId = 'user' in (req as any) ? (req as any).user.id : null;
+        if (userID === null) {
+          return next(new Error('Expected bson.ObjectId in Request.user.id'));
+        }
+        this.api.users.findById(userID).then(user => {
+          if (!user) {
+            return next(new Error(`User with id ${userID} not found`));
           }
-          if (!achievement) {
-            return next(new Error('action not found'));
+          // check operation
+          switch (achievement.requiredCondition.statistic) {
+            case Statistic.complete: { // binary boolean business
+
+            }
+            case Statistic.totalCount: { // ++
+
+            }
+            case Statistic.uniqueCount: { // ++ if data not in user -> achievement -> data already
+
+            }
+            case Statistic.value: { // ? idk why this exists i forget. something to do with specific values
+
+            }
           }
+          switch (achievement.requiredCondition.operator) {
+            case Op.eq: {
 
-        /**
-         * check for achievement action URL
-         *    check for user account
-         *        check operation
-         *            update user achievement instance
-         *                trigger event if it gets unlocked
-         */
+            }
+            case Op.ne: {
 
-          return next();
-      });
+            }
+            case Op.gt: {
+
+            }
+            case Op.gte: {
+
+            }
+            case Op.lt: {
+
+            }
+            case Op.lte: {
+
+            }
+          }
+        }).catch(err2 => next(err2));
+
+      /**
+       * check for achievement action URL //
+       *    check for user account //
+       *        check operation
+       *            update user achievement instance
+       *                trigger event if it gets unlocked
+       */
+
+        return next();
+
+      }).catch(err => next(err));
     };
 
     this.router.all(`${config.scope}*`, middleware);
--- src/api/achievements/AchievementsAPI.ts
+++ src/api/achievements/AchievementsAPI.ts
@@ -18,4 +18,42 @@ export default class AchievementsAPI extends API {
     });
   }
 
+  public findActiveAchievementByAction(action: string): Promise<IAchievementNS.IAchievementShape> {
+    return new Promise<IAchievementNS.IAchievementShape>((resolve, reject) => {
+      this._model.findOne(
+        {
+          'meta.isActive': true,
+          '$and': [
+            {
+              $or: [
+                {
+                  'meta.expiration': {
+                    $lte: new Date(),
+                  },
+                },
+                {
+                  'meta.expiration': null,
+                },
+              ],
+            },
+            {
+              $or: [
+                {
+                  action,
+                },
+                {
+                  action: `/${action}`,
+                },
+              ],
+            },
+          ],
+        },
+      ).then(achievement => {
+        resolve(achievement);
+      }).catch(err => {
+        reject(err);
+      });
+    });
+  }
+
 }
